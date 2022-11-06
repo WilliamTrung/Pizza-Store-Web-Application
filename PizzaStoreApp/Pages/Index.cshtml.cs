93e518ef7fe7e99d0462793a8e737f4e2e51f82c
phunt <phungothanh784@gmail.com>
2022-11-06 13:46:34 +0700

    edit FE

--- PizzaStoreApp/Pages/Index.cshtml.cs
+++ PizzaStoreApp/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessService.DTOs;
 using BusinessService.IService;
+using BusinessService.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using PizzaStoreApp.Helper;
@@ -10,11 +11,18 @@ namespace PizzaStoreApp.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly IProductService productService;
+        private readonly ICategoryService categoryService;
+        public IndexModel(ILogger<IndexModel> logger, IProductService _productService, ICategoryService _categoryService)
         {
             _logger = logger;
+            productService = _productService;
+            categoryService = _categoryService;
         }
-        public IActionResult OnGet()
+        public Product product { get; set; }
+        public IList<Product> Products { get; set; }
+        public IList<Category> Categories { get; set; }
+        /*public IActionResult OnGet()
         {
             if(SessionExtension.GetLoginUser(HttpContext.Session) == null)
             {
@@ -22,6 +30,24 @@ namespace PizzaStoreApp.Pages
             } else
             {
                 return RedirectToPage("Management/Products/Index");
+            }
+        }*/
+        public async Task OnGetAsync()
+        {
+            try
+            {
+                var products = await productService.GetDTOs(includeProperties: "Category,Supplier");
+                Products = (IList<Product>)products.ToList();
+                Products = Products.ToList();
+         /*       Products = Products.OrderBy(d => d.ProductId).Take(6).ToList();*/
+
+                var categories = await categoryService.GetDTOs();
+                Categories = (IList<Category>)categories.ToList();
+                Categories = Categories.ToList();
+            }
+            catch (Exception ex)
+            {
+                TempData["Message"] = ex.Message;
             }
         }
     }
